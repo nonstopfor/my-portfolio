e5b8275ef33b9874cfd586538c49ed7aac9f09d2
Zhexin <2481389122@qq.com>
2020-07-07 21:21:55 +0800

    finish week2 toturial, add comments and datastore

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -22,24 +22,50 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-  private ArrayList<String> comments=new ArrayList<String>();
+  //private ArrayList<String> comments=new ArrayList<String>();
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String comment=request.getParameter("comment");
-    comments.add(comment);
+    //comments.add(comment);
+    Entity commentEntity=new Entity("comment");
+    long timestamp = System.currentTimeMillis();
+
+    commentEntity.setProperty("comment",comment);
+    commentEntity.setProperty("timestamp", timestamp);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentEntity);
+
     response.sendRedirect("/index.html");
   }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     
+    ArrayList<String>comments=new ArrayList<String>();
+    Query query=new Query("comment").addSort("timestamp", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    PreparedQuery results=datastore.prepare(query);
+    for(Entity entity: results.asIterable()){
+        comments.add((String)entity.getProperty("comment"));
+    }
+
     Gson gson = new Gson();
     
+
     String json=gson.toJson(comments);
     
     response.setContentType("application/json;");

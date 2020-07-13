e07c3a2607064c062d675495da3b1fc413eac291
Zhexin <2481389122@qq.com>
2020-07-13 11:24:02 +0800

    finish week4 work

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,63 @@
 
 package com.google.sps;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.List;
+import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<String> Request_attendees = request.getAttendees();
+    List<TimeRange> res = new LinkedList<>();
+    res.add(TimeRange.WHOLE_DAY);
+    for (Event e : events) {
+      Set<String> Attendees = e.getAttendees();
+      boolean flag = false;
+      for (String a : Attendees) {
+        if (Request_attendees.contains(a)) {
+          flag = true;
+          break;
+        }
+      }
+      if (!flag)
+        continue;
+      TimeRange bad = e.getWhen();
+      ListIterator it = res.listIterator();
+      while (it.hasNext()) {
+        TimeRange t = (TimeRange) it.next();
+        if (t.start() >= bad.start() + bad.duration())
+          continue;
+        if (t.start() < bad.start()) {
+          if (bad.start() >= t.start() + t.duration()) {
+            continue;
+          } else if (bad.start() + bad.duration() >= t.start() + t.duration()) {
+            it.set(TimeRange.fromStartDuration(t.start(), bad.start() - t.start()));
+          } else {
+            it.set(TimeRange.fromStartDuration(t.start(), bad.start() - t.start()));
+            it.add(TimeRange.fromStartDuration(bad.start() + bad.duration(),
+                t.start() + t.duration() - bad.start() - bad.duration()));
+          }
+        } else {
+          if (bad.start() + bad.duration() >= t.start() + t.duration()) {
+            it.remove();
+          } else {
+            it.set(TimeRange.fromStartDuration(bad.start() + bad.duration(),
+                t.start() + t.duration() - bad.start() - bad.duration()));
+          }
+        }
+      }
+
+    }
+    ListIterator it = res.listIterator();
+    while (it.hasNext()) {
+      TimeRange t = (TimeRange) it.next();
+      if (t.duration() < request.getDuration()) {
+        it.remove();
+      }
+    }
+    return res;
   }
 }
